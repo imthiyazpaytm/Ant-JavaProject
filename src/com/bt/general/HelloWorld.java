942607c9c55b8e1cbdffc8432e1166edc05d49d0
imtiyaz132 <41620854+imtiyaz132@users.noreply.github.com>
2018-09-04 22:51:11 +0530

    Update HelloWorld.java

--- src/com/bt/general/HelloWorld.java
+++ src/com/bt/general/HelloWorld.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to ANT11 !! " + getLocalCurrentDate());
+		System.out.println("Hello Welcome to ANT11i !! " + getLocalCurrentDate());
 	}
 
 	private static Date getLocalCurrentDate() {
